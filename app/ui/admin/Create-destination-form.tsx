233415b2b3a604533a6428a4285a06a1071876e0
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2023-12-08 12:12:20 -0400

    feat: add create destination post action

--- app/ui/admin/Create-destination-form.tsx
+++ app/ui/admin/Create-destination-form.tsx
@@ -1,11 +1,19 @@
+"use client";
+
+import { createDestination } from "@/app/lib/actions";
+import { useFormState } from "react-dom";
+
 export default function CreateDestinationForm() {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createDestination, initialState);
+
   return (
     <div className="rounded-xl bg-[#EDEDED] py-8 px-10 mb-6">
       <h2 className="text-2xl font-bold text-[#242424] mb-2">
         Create destination
       </h2>
 
-      <form action="">
+      <form action={dispatch}>
         <div className="grid grid-cols-2 gap-4">
           <div>
             <div className="font-bold text-[#696969] text-xl">
@@ -15,9 +23,17 @@ export default function CreateDestinationForm() {
               type="text"
               name="country"
               id="country"
-              required
               className="p-3 w-full bg-[#D9D9D9] mt-2"
+              aria-describedby="country-error"
             />
+            <div id="country-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.country &&
+                state.errors.country.map((error: string) => (
+                  <p className="mt-2 text-base text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
 
           <div>
@@ -29,8 +45,16 @@ export default function CreateDestinationForm() {
               name="city"
               id="city"
               className="p-3 w-full bg-[#D9D9D9] mt-2"
-              required
+              aria-describedby="city-error"
             />
+            <div id="city-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.city &&
+                state.errors.city.map((error: string) => (
+                  <p className="mt-2 text-base text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
 
           <div>
@@ -42,15 +66,36 @@ export default function CreateDestinationForm() {
               name="price"
               id="price"
               className="p-3 w-full bg-[#D9D9D9] mt-2"
-              required
+              aria-describedby="price-error"
             />
+            <div id="price-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.price &&
+                state.errors.price.map((error: string) => (
+                  <p className="mt-2 text-base text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
 
           <div>
             <div className="font-bold text-[#696969] text-xl">
               <label htmlFor="date">Description</label>
             </div>
-            <textarea name="description" id="description" className="p-3 w-full bg-[#D9D9D9] mt-2"></textarea>
+            <textarea
+              name="description"
+              id="description"
+              className="p-3 w-full bg-[#D9D9D9] mt-2"
+              aria-describedby="description-error"
+            ></textarea>
+            <div id="description-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.description &&
+                state.errors.description.map((error: string) => (
+                  <p className="mt-2 text-base text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
 
           <div>
@@ -62,9 +107,25 @@ export default function CreateDestinationForm() {
               name="score"
               id="score"
               className="p-3 w-full bg-[#D9D9D9] mt-2"
-              required
+              aria-describedby="score-error"
             />
+            <div id="score-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.score &&
+                state.errors.score.map((error: string) => (
+                  <p className="mt-2 text-base text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
+        </div>
+
+        <div aria-live="polite" aria-atomic="true">
+          {state.errors && (
+            <p className="mt-2 text-md text-red-500" key={state.message}>
+              {state.message}
+            </p>
+          )}
         </div>
 
         <div className="flex justify-end mt-6 space-x-4">
