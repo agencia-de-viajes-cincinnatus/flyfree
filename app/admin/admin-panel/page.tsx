a76ba2c965efc34ed3479534862a54839536f707
juniorangeles <yuniorangeles06@gmail.com>
2023-12-26 20:31:49 -0400

    feat: rendering components in page

--- app/admin/admin-panel/page.tsx
+++ app/admin/admin-panel/page.tsx
@@ -3,7 +3,7 @@ import AdminPanelDestination from '@/app/ui/admin/admin-panel/admin-panel-destin
 import AdminPanelReservation from '@/app/ui/admin/admin-panel/admin-panel-reservation';
 export default function AdminPanelPage() {
   return (
-    <div>
+    <div className='flex justify-center space-x-8'>
       <AdminPanelReservation />
       <AdminPanelDestination />
     </div>
