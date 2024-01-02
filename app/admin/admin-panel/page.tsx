e3566d4c94a3ac9b817cc0e8846785ab4a4957b4
juniorangeles <yuniorangeles06@gmail.com>
2024-01-02 15:16:14 -0400

    chore: render new componets

--- app/admin/admin-panel/page.tsx
+++ app/admin/admin-panel/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import AdminPanelDestination from '@/app/ui/admin/admin-panel/admin-panel-destination';
 import AdminPanelReservation from '@/app/ui/admin/admin-panel/admin-panel-reservation';
+import AdminPanelUsers from '@/app/ui/admin/admin-panel/admin-panel-users';
 export default function AdminPanelPage() {
   return (
-    <div className='flex justify-center space-x-8'>
-      <AdminPanelReservation />
-      <AdminPanelDestination />
-    </div>
+    <>
+      <div>
+        {/* AdminPanelUsers arriba y con ancho completo */}
+        <AdminPanelUsers />
+
+        {/* AdminPanelReservation y AdminPanelDestination debajo y en paralelo */}
+
+        <div className='flex flex-row'>
+          <AdminPanelReservation />
+          <AdminPanelDestination />
+        </div>
+      </div>
+    </>
   );
 }
