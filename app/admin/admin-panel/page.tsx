8d8db9b404b97a6135dd588d51c191a03e439682
juniorangeles <yuniorangeles06@gmail.com>
2024-01-03 00:40:09 -0400

    chore: new way to render

--- app/admin/admin-panel/page.tsx
+++ app/admin/admin-panel/page.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import AdminPanelDestination from '@/app/ui/admin/admin-panel/admin-panel-destination';
-import AdminPanelReservation from '@/app/ui/admin/admin-panel/admin-panel-reservation';
-import AdminPanelUsers from '@/app/ui/admin/admin-panel/admin-panel-users';
+import AdminPanelDestination from '@/app/ui/admin/admin-panel/admin-panel-destination-card';
+import AdminPanelReservation from '@/app/ui/admin/admin-panel/admin-panel-reservation-card';
+import AdminPanelUsers from '@/app/ui/admin/admin-panel/admin-panel-users-card';
 export default function AdminPanelPage() {
   return (
     <>
       <div>
-        {/* AdminPanelUsers arriba y con ancho completo */}
         <AdminPanelUsers />
-
-        {/* AdminPanelReservation y AdminPanelDestination debajo y en paralelo */}
-
-        <div className='flex flex-row'>
-          <AdminPanelReservation />
-          <AdminPanelDestination />
-        </div>
+        <AdminPanelReservation />
+        <AdminPanelDestination />
       </div>
     </>
   );
