afdecd6caa759c5ee15784441d8cc670e358df5b
juniorangeles <yuniorangeles06@gmail.com>
2024-01-05 11:55:15 -0400

    feat: add styles of card preferens

---
+++ app/ui/client/profile/PreferensTravelCardProfile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { travels } from '@/app/lib/data';
+
+export default function PreferensTravelCardProfile() {
+  return (
+    <div>
+      {travels.map((travel, index) => (
+        <div key={index} className='bg-gray-100 rounded-lg shadow-md p-6 mb-4'>
+          <div className='flex justify-between'>
+            <div className='flex'>
+              <div className='mr-4'>
+                <img
+                  className='w-16 h-16 rounded-full object-cover'
+                  src={travel.avatar}
+                  alt='travel'
+                />
+              </div>
+              <div>
+                <h1 className='font-bold text-xl'>{travel.city}</h1>
+                <p className='text-gray-500 text-sm'>{travel.country}</p>
+              </div>
+            </div>
+            <div>
+              <p className='text-gray-500 text-sm'>{travel.date}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
