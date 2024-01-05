224e2c53c73d90649cce48952e1403305adb9a82
juniorangeles <yuniorangeles06@gmail.com>
2024-01-05 11:53:26 -0400

    feat: add page only to travel history

---
+++ app/client/travel-history/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HistoriTravel from '@/app/ui/client/travel-history/HistoriTravel';
+
+export default function HistoriTravelPage() {
+  return (
+    <>
+      <HistoriTravel />
+    </>
+  );
+}
