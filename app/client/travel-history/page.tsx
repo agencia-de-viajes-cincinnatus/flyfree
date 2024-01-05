d7e8e202ea69358e394249f7feca7d1c9dca8c38
juniorangeles <yuniorangeles06@gmail.com>
2024-01-05 14:39:46 -0400

    chore: proof

--- app/client/travel-history/page.tsx
+++ app/client/travel-history/page.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import HistoriTravel from '@/app/ui/client/travel-history/HistoriTravel';
-
 export default function HistoriTravelPage() {
-  return (
-    <>
-      <HistoriTravel />
-    </>
-  );
+  return <>{/* <HistoriTravel /> */}</>;
 }
