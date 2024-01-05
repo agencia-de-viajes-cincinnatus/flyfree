1f88e247fd9868a9d1b287c30468e3cbb2908c43
juniorangeles <yuniorangeles06@gmail.com>
2024-01-05 14:40:18 -0400

    chore: new class

--- app/ui/client/profile/PreferensTravelCardProfile.tsx
+++ app/ui/client/profile/PreferensTravelCardProfile.tsx
@@ -5,23 +5,21 @@ export default function PreferensTravelCardProfile() {
   return (
     <div>
       {travels.map((travel, index) => (
-        <div key={index} className='bg-gray-100 rounded-lg shadow-md p-6 mb-4'>
+        <div key={index} className='bg-gray-100 rounded-lg shadow-md p-4 mb-4'>
           <div className='flex justify-between'>
-            <div className='flex'>
+            <div className='flex items-center'>
               <div className='mr-4'>
                 <img
-                  className='w-16 h-16 rounded-full object-cover'
+                  className='w-30 h-25 rounded-lg object-cover'
                   src={travel.avatar}
                   alt='travel'
                 />
               </div>
               <div>
-                <h1 className='font-bold text-xl'>{travel.city}</h1>
+                <h1 className='font-bold text-lg'>{travel.city}</h1>
                 <p className='text-gray-500 text-sm'>{travel.country}</p>
+                <p className='text-gray-700 text-sm'>{travel.description}</p>
               </div>
-            </div>
-            <div>
-              <p className='text-gray-500 text-sm'>{travel.date}</p>
             </div>
           </div>
         </div>
