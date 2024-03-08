5b3e6426324de25478c723cc6d13fda4ba4e86d7
juniorangeles <yuniorangeles06@gmail.com>
2024-03-07 23:00:21 -0400

    feat: add new pakaje

--- app/ui/register/form.tsx
+++ app/ui/register/form.tsx
@@ -1,9 +1,15 @@
+'use client';
 import React from 'react';
 import { Input } from '@/app/ui/input';
 import { Button } from '@/app/ui/button';
+import { registerUser } from '@/app/lib/action';
 import Image from 'next/image';
+import { useFormState, useFormStatus } from 'react-dom';
+import { ExclamationCircleIcon } from '@heroicons/react/20/solid';
 
 export default function RegisterForm() {
+  const [state, dispatch] = useFormState(registerUser, undefined);
+
   return (
     <div className='mx-auto max-w-md rounded-md bg-gradient-to-tr  from-gray-50 to-gray-95 p-6 shadow-md ml-0'>
       <div className='flex justify-center items-center h-[25vh] '>
@@ -17,7 +23,7 @@ export default function RegisterForm() {
           />
         </div>
       </div>
-      <form className='flex-col flex'>
+      <form action={dispatch} className='flex-col flex'>
         <div>
           <label htmlFor='' className='block text-sm font-medium text-gray-700'>
             Nombre Completo del Usuario
@@ -25,6 +31,7 @@ export default function RegisterForm() {
           <Input
             type='text'
             name='username'
+            id='username'
             className='mt-1 w-full rounded-md border border-gray-300 p-3 focus:border-blue-300 focus:outline-none focus:ring'
           />
         </div>
@@ -34,7 +41,8 @@ export default function RegisterForm() {
           </label>
           <Input
             type='text'
-            name='username'
+            name='email'
+            id='email'
             className='mt-1 w-full rounded-md border border-gray-300 p-3 focus:border-blue-300 focus:outline-none focus:ring'
           />
         </div>
@@ -45,7 +53,8 @@ export default function RegisterForm() {
           <Input
             type='password'
             name='password'
-            className='mt-1 w-full rounded-md  border border-gray-300 p-3 focus:border-blue-300 focus:outline-none focus:ring'
+            id='password'
+            className='mt-1 w-full rounded-md border border-gray-300 p-3 focus:border-blue-300 focus:outline-none focus:ring'
           />
         </div>
         <div>
@@ -54,9 +63,22 @@ export default function RegisterForm() {
           </label>
           <Input
             type='password'
-            name='password'
-            className='mt-1 w-full rounded-md  border border-gray-300 p-3 focus:border-blue-300 focus:outline-none focus:ring'
+            name='validatepassword'
+            id='validatepassword'
+            className='mt-1 w-full rounded-md border border-gray-300 p-3 focus:border-blue-300 focus:outline-none focus:ring'
           />
+        </div>
+        <div
+          className='flex h-8 items-end space-x-1'
+          aria-live='polite'
+          aria-atomic='true'
+        >
+          {state === 'PasswordsMismatch' && (
+            <>
+              <ExclamationCircleIcon className='h-5 w-5 text-red-500' />
+              <p className='text-sm text-red-500'>Contraceña no coincide</p>
+            </>
+          )}
         </div>
         <div>
           <Button
