11e123d4944652c75e0347880faa2a612e13c4df
juniorangeles <yuniorangeles06@gmail.com>
2024-03-10 13:52:49 -0400

    feat: add tada profile

--- app/ui/client/profile/UserProfiler.tsx
+++ app/ui/client/profile/UserProfiler.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
+'use client';
+import { useState, useEffect } from 'react';
 import { fetchProfileData } from '@/app/lib/data';
-export default async function UserProfiler() {
-  const user = await fetchProfileData();
-  console.log(user);
-  return (
-    <div className='bg-white p-4 rounded-lg shadow-md w-72 h-screen'>
-      <div className='flex flex-col items-center'>
-        <img src='' alt='Avatar' className='w-35 h-35 rounded-full mb-2' />
-        <div className='text-center mt-2'>
-          <h2 className='text-xl font-semibold'>uo</h2>
-          <p className='text-lg text-gray-600'>uo</p>
-        </div>
-      </div>
 
-      <div className='mt-4'>
-        <p className='text-lg text-gray-700'>io</p>
-      </div>
+import useDatauser from '@/app/hooks/useDataCookie';
+
+function Profile() {
+  const { user } = useDatauser();
+
+  return (
+    <div>
+      <h1>Perfil</h1>
     </div>
   );
 }
+
+export default Profile;
